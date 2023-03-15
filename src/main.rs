5e7ffbf74f2ed6a93d26a9182c54ede91055a8d9
Bea Boxler <bea@boxler.me>
2023-03-15 03:39:53 +0000

    Time logger ⍶

---
+++ src/main.rs
@@ -0,0 +1,166 @@
+mod categories;
+mod preferences;
+
+use categories::categories;
+use cursive::theme::BaseColor::Green;
+use cursive::theme::Color::Dark;
+use std::cell::RefCell;
+use std::rc::Rc;
+
+use cursive::view::{Nameable, Resizable};
+use cursive::views::{Dialog, EditView, LinearLayout, Panel, SelectView, TextView, ViewRef};
+use cursive::{Cursive, CursiveExt};
+use preferences::Preferences;
+
+const WIDTH: usize = 86;
+
+type PrefRef = Rc<RefCell<Preferences>>;
+
+fn main() {
+    let prefs = Rc::new(RefCell::new(
+        Preferences::load().unwrap_or(Preferences::new()),
+    ));
+
+    let mut c = Cursive::new();
+    c.add_global_callback('q', |c| match c.pop_layer() {
+        Some(_) => c.noop(),
+        None => c.quit(),
+    });
+
+    c.update_theme(|theme| theme.palette.set_color("Background", Dark(Green)));
+    c.set_window_title("Jogger");
+    c.add_layer(create_menu_dialog(prefs).fixed_width(WIDTH));
+
+    c.run();
+}
+
+fn create_menu_dialog(prefs: PrefRef) -> Dialog {
+    let menu = SelectView::new()
+        .item("Log Time to a Ticket", 1)
+        .item("Log Personal Distraction", 2)
+        .item("Setup", 3)
+        .item("Quit", 4)
+        .on_submit(move |c, item| {
+            let prefs = prefs.clone();
+            match item {
+                2 => c.add_layer(create_time_log_dialog(
+                    prefs,
+                    Some("Log Personal Distraction"),
+                ).fixed_width(WIDTH)),
+                3 => c.add_layer(create_setup_dialog(prefs).fixed_width(WIDTH)),
+                4 => c.quit(),
+                _ => c.add_layer(
+                    Dialog::around(TextView::new("This function has not yet been implemented."))
+                        .button("Okay", |c| {
+                            c.pop_layer();
+                        }),
+                ),
+            }
+        });
+
+    Dialog::around(menu).title("Jogger")
+}
+
+fn create_time_log_dialog(_: PrefRef, title: Option<&str>) -> Dialog {
+    let mut categories_view = SelectView::new();
+    for category in categories().keys().into_iter() {
+        categories_view.add_item_str(*category);
+    }
+    categories_view.set_on_select(|c, item| {
+        let mut action_view = c.find_name("actions").unwrap() as ViewRef<SelectView>;
+        action_view.clear();
+        let actions: Vec<&str> = categories()
+            .get(item.as_str())
+            .unwrap()
+            .iter()
+            .map(|v| *v)
+            .collect();
+        action_view.add_all_str(actions);
+    });
+
+    let actions_view = SelectView::new().item_str("Please Select a Category");
+
+    let view = LinearLayout::vertical()
+        .child(
+            LinearLayout::horizontal()
+                .child(Panel::new(categories_view.with_name("category")).fixed_width(WIDTH / 2))
+                .child(Panel::new(actions_view.with_name("actions")).fixed_width(WIDTH / 2)),
+        )
+        .child(
+            LinearLayout::horizontal()
+                .child(TextView::new("Comment: "))
+                .child(EditView::new().full_width()),
+        );
+
+    Dialog::around(view)
+        .title(title.unwrap_or("Create Time Log"))
+        .button("Submit", |_| ())
+        .button("Cancel", |c| {
+            c.pop_layer();
+        })
+}
+
+fn create_setup_dialog(prefs: PrefRef) -> Dialog {
+    let prefs = prefs.clone();
+    let layout = LinearLayout::vertical()
+        .child(
+            LinearLayout::horizontal()
+                .child(TextView::new("Your Name: "))
+                .child(
+                    EditView::new()
+                        .content(&prefs.borrow().name)
+                        .with_name("name")
+                        .full_width(),
+                ),
+        )
+        .child(
+            LinearLayout::horizontal()
+                .child(TextView::new("API Key: "))
+                .child(
+                    EditView::new()
+                        .content(&prefs.borrow().api_key)
+                        .with_name("api_key")
+                        .full_width(),
+                ),
+        )
+        .child(
+            LinearLayout::horizontal()
+                .child(TextView::new("Personal Distraction Ticket: "))
+                .child(
+                    EditView::new()
+                        .content(&prefs.borrow().personal_distraction)
+                        .with_name("personal_distraction")
+                        .full_width(),
+                ),
+        );
+
+    Dialog::around(layout)
+        .button("Save", move |c| {
+            let prefs = prefs.clone();
+            let name = &*(c.find_name("name").unwrap() as ViewRef<EditView>).get_content();
+            let api_key = &*(c.find_name("api_key").unwrap() as ViewRef<EditView>).get_content();
+            let personal_distraction =
+                &*(c.find_name("personal_distraction").unwrap() as ViewRef<EditView>).get_content();
+
+            prefs
+                .borrow_mut()
+                .set_name(name)
+                .set_api_key(api_key)
+                .set_personal_distraction(personal_distraction);
+
+            match prefs.clone().borrow().save() {
+                Ok(_) => {
+                    c.pop_layer();
+                }
+                Err(err) => c.add_layer(Dialog::around(TextView::new(format!(
+                    "An error occured: {}",
+                    err
+                )))),
+            }
+        })
+        .button("Cancel", |c| {
+            c.pop_layer();
+        })
+        .title("Setup")
+        .padding_top(1)
+}
---
+++ src/preferences.rs
@@ -0,0 +1,66 @@
+#![allow(deprecated)]
+
+use std::io::Error;
+use std::env::home_dir;
+
+
+const PREF_FILENAME: &str = "jogger.conf";
+
+#[derive(Debug, Clone)]
+pub struct Preferences {
+    pub name: String,
+    pub api_key: String,
+    pub personal_distraction: String,
+}
+
+impl Preferences {
+    pub fn new() -> Self {
+        Preferences { name: String::new(), api_key: String::new(), personal_distraction: String::new() }
+    }
+
+    pub fn load() -> Result<Self, Error> {
+        let input = std::fs::read_to_string(home_dir().unwrap_or_default().join(".config").join(PREF_FILENAME))?;
+        let mut prefs = Preferences::new();
+        for line in input.lines() {
+            match line.split_once("=").unwrap() {
+                ("NAME", name) => {prefs.set_name(name);},
+                ("API_KEY", api_key) => {prefs.set_api_key(api_key);},
+                ("PERSONAL_DISTRACTION", personal_distraction) => {prefs.set_personal_distraction(personal_distraction);},
+                _ => {}
+            }
+        };
+
+        Ok(prefs)
+    }
+
+    pub fn set_name(&mut self, name: &str) -> &mut Self {
+        self.name = name.to_string();
+        self
+    }
+
+    pub fn set_personal_distraction(&mut self, personal_distraction: &str) -> &mut Self {
+        self.personal_distraction = personal_distraction.to_string();
+        self
+    }
+
+    pub fn set_api_key(&mut self, api_key: &str) -> &mut Self {
+        self.api_key = api_key.to_string();
+        self
+    }
+
+    pub fn save(&self) -> Result<(), Error> {
+        let path = home_dir().unwrap_or_default().join(".config");
+        std::fs::create_dir_all(&path)?;
+        std::fs::write(path.join(PREF_FILENAME), format!("{self}").as_bytes())?;
+
+        Ok(())
+    }
+}
+
+impl std::fmt::Display for Preferences {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "NAME={}", self.name)?;
+        writeln!(f, "API_KEY={}", self.api_key)?;
+        writeln!(f, "PERSONAL_DISTRACTION={}", self.personal_distraction)
+    }
+}
